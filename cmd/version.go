019c3b06e1179ff04e4c101410fea04bd2e4d413
Vladimir Parfenov <parfenov.vladimir.s@gmail.com>
2025-06-29 12:37:19 +0300

    Update version information to 0.1.3 and simplify output

--- cmd/version.go
+++ cmd/version.go
@@ -7,24 +7,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var Version = "0.1.0-dev"
-var BuildDate = "unknown"
-var GitCommit = "unknown"
+var Version = "0.1.3"
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Show version information",
-	Long:  `Display version, build date, and git commit information for LogLion.`,
+	Long:  `Display version for LogLion.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		logrus.WithFields(logrus.Fields{
 			"version":    Version,
-			"build_date": BuildDate,
-			"git_commit": GitCommit,
 		}).Debug("Displaying version information")
 
-		fmt.Printf("LogLion version %s\n", Version)
-		fmt.Printf("Build date: %s\n", BuildDate)
-		fmt.Printf("Git commit: %s\n", GitCommit)
+		fmt.Println(Version)
 	},
 }
 
